bcb7fe702c7297c82bbb090f1159043d3dea7f04
Pietro Gambatesa <73944807+TheiPhoneDev@users.noreply.github.com>
2024-11-01 21:18:08 +0100

    Update UpdateViewHandler.swift

--- UpdateKit/Sources/UpdateKit/UpdateViewHandler.swift
+++ UpdateKit/Sources/UpdateKit/UpdateViewHandler.swift
@@ -99,42 +99,6 @@ public struct UpdateViewHandler: View {
     }
 }
 
-public class Typography: ObservableObject {
-    @Published public var sfSymbolFontType: Font = .title.weight(.semibold)
-    @Published public var textTitleFontType: Font = .title2.weight(.semibold)
-    @Published public var textDescriptionFontType: Font = .subheadline
-    @Published public var textDescriptionFontColor: Color = .secondary
-    @Published public var buttonColor: UIColor = .systemBlue
-    @Published public var imageColor: UIColor = .systemGray
-    @Published public var imageWidth: CGFloat = 40
-    @Published public var imageHeight: CGFloat = 40
-
-    public init() {}
-
-}
-
-public class UpdatePresenter: ObservableObject {
-    @AppStorage("isPresentable") public var isPresentable: Bool = true
-    public init() {}
-
-    @MainActor
-    public func setTransparentNavBar() {
-        let appearance = UINavigationBarAppearance()
-        appearance.configureWithTransparentBackground()
-
-        UINavigationBar.appearance().scrollEdgeAppearance = appearance
-    }
-
-    @MainActor
-    public func resetNavBarAppearance() {
-        let appearance = UINavigationBarAppearance()
-        appearance.configureWithDefaultBackground()
-
-        UINavigationBar.appearance().standardAppearance = appearance
-        UINavigationBar.appearance().scrollEdgeAppearance = appearance
-    }
-    
-}
 
 
 
