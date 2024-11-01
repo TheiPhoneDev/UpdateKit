800cf50a3a3171bf497e3af6d4fd134a5e6dc168
Pietro Gambatesa <73944807+TheiPhoneDev@users.noreply.github.com>
2024-11-01 21:20:44 +0100

    Create UpdateViewExamples.swift

---
+++ UpdateKit/Sources/UpdateKit/UpdateViewExamples.swift
@@ -0,0 +1,27 @@
+//
+//  UpdateViewExamples.swift
+//  UpdateKit
+//
+//  Created by Pietro Gambatesa on 11/1/24.
+//
+
+import Foundation
+import SwiftUI
+
+struct TestView1: View {
+    var body: some View {
+        Text("Hello View 1")
+    }
+}
+
+struct TestView2: View {
+    var body: some View {
+        Text("Hello View 2")
+    }
+}
+
+struct TestView3: View {
+    var body: some View {
+        Text("Hello View 3")
+    }
+}
